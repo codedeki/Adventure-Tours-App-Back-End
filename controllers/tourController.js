10bda5834e820b71264231dd2e6f678a5d90f467
codedeki <serbian_5@hotmail.com>
2020-05-26 12:38:05 -0400

    Implementing Mongoose Driver

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,18 +1,6 @@
-const fs = require('fs');
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
+const Tour = require('../models/tourModel');
 
-//middleware controller
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour id is: ${val}`);
-
-  if (req.params.id * 1 > tours.length) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-  next();
-};
+// const tours = JSON.parse(fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`));
 
 //middleware controller
 exports.checkBody = (req, res, next) => {
@@ -27,49 +15,32 @@ exports.checkBody = (req, res, next) => {
 
 //functions
 exports.getAllTours = (req, res) => {
-  console.log(req.requestTIme);
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
-    requestedAt: req.requestTIme,
-    results: tours.length,
+    requestedAt: req.requestTime,
+    // results: tours.length,
     data: {
-      tours: tours,
+      // tours: tours,
     },
   });
 };
 
 exports.getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1; //change string to # by multiply a string which does a type coersion!
-  const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
   res.status(200).json({
     status: 'success',
     data: {
-      tour: tour,
+      // tour: tour,
     },
   });
 };
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
-    res.status(201).json({
-      status: 'success',
-      data: {
-        tour: newTour,
-      },
-    });
+  res.status(201).json({
+    status: 'success',
+    // data: {
+    //   tour: newTour,
+    // },
   });
 };
 
@@ -82,7 +53,7 @@ exports.updateTour = (req, res) => {
   });
 };
 
-exports.deleteTour = () => {
+exports.deleteTour = (req, res) => {
   res.status(204).json({
     status: 'success',
     data: null,
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const tourController = require('./../controllers/tourController');
+const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID); //set up middleware filter for our tour functions below w/ param
+// router.param('id', tourController.checkID ); //set up middleware filter for our tour functions below w/ param
 router.route('/').get(tourController.getAllTours).post(tourController.checkBody, tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
