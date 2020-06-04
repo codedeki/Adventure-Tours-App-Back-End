255ff6a3ebed453f74f96f8bbfc04f7db57fdeaa
codedeki <serbian_5@hotmail.com>
2020-06-03 20:34:45 -0400

    Error Handling: Development vs Production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,37 @@
+const sendErorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('ERROR', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Oops, something went very wrong!',
+    });
+  }
+};
+
 //Middleware Error Handler: 4 params are automatically recognized as error handler middleware by Express
 module.exports = (err, req, res, next) => {
   // console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
