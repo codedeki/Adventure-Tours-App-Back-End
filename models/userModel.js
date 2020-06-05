f1ce3d893696159c11b935b8985aefd792b38150
codedeki <serbian_5@hotmail.com>
2020-06-05 18:57:24 -0400

    Mongoose: Model Users & Create New Users to DB

---
+++ models/userModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your username!'],
+    unique: true,
+    trim: true,
+    maxlength: [40, 'A username cannot be longer than 40 characters'],
+    minlength: [10, 'A username must be longer than 10 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, 'Please provide a valid e-mail address'],
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
