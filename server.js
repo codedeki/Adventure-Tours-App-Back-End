a60b29af88fd014ba8290a3879829988e93ea93b
codedeki <serbian_5@hotmail.com>
2020-06-04 20:39:10 -0400

    Error Handling: Unhandled Rejections & Uncaught Exceptions

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+//Sync Error handling: Uncaught exceptions: Put sync at top of file to run before all other lines of code
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
@@ -24,6 +31,15 @@ mongoose
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+//Async Error handling: last safety net with process.on
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1); //shut down app if error (0 = success, 1= uncaught exception)
+  });
 });
