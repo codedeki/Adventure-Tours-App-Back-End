ae4552791b34966f5a15077de7ebcdb37c37347e
codedeki <serbian_5@hotmail.com>
2020-06-20 18:00:04 -0400

    Refactor Auth & Auth Routes & Importing Review & User Data

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -24,17 +24,31 @@ router
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router.route('/tour-stats').get(tourController.getTourStats);
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+router
+  .route('/monthly-plan/:year')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
+    tourController.getMonthlyPlan
+  );
 
 // router.param('id', tourController.checkID ); //set up middleware filter for our tour functions below w/ param
 router
   .route('/')
-  .get(/*(CatchAsync*/ authController.protect, tourController.getAllTours)
-  .post(tourController.createTour);
+  .get(/*(CatchAsync*/ tourController.getAllTours)
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.createTour
+  );
 router
   .route('/:id')
-  .get(authController.protect, tourController.getTour)
-  .patch(tourController.updateTour)
+  .get(tourController.getTour)
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.updateTour
+  )
   .delete(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,24 +13,17 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword
-);
+router.use(authController.protect); // protect all router routes after this line so don't have to rewrite same code
 
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin')); //only admin has access to routes below after this line
 
 router
   .route('/')
